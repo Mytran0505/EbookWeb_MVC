017c5fefb5c283a3f96329d922b0ebcc3a6fa13a
MyTran0505 <mytran0505itlker@gmail.com>
2023-11-02 23:39:20 +0700

    push

--- EbookWeb/Controllers/CategoryController.cs
+++ EbookWeb/Controllers/CategoryController.cs
@@ -20,5 +20,79 @@ namespace EbookWeb.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid) {
+                _context.Categories.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _context.Categories.Find(id);
+            /*Category? categoryFromDb1 = _context.Categories.FirstOrDefault(u=>u.Id==id);
+            Category? categoryFromDb2 = _context.Categories.Where(u => u.Id == id).FirstOrDefault();*/
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _context.Categories.Find(id);
+            /*Category? categoryFromDb1 = _context.Categories.FirstOrDefault(u=>u.Id==id);
+            Category? categoryFromDb2 = _context.Categories.Where(u => u.Id == id).FirstOrDefault();*/
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _context.Categories.Find(id);
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
