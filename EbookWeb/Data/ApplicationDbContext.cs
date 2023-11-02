6dff43a207553cf691d2946094cb6fcb568116cc
MyTran0505 <mytran0505itlker@gmail.com>
2023-11-02 14:55:22 +0700

    category

---
+++ EbookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EbookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EbookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+            
+        } 
+
+        public DbSet<Category> Categories {  get; set; }
+    }
+}
